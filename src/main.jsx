542a66dd36509596bb4c078c80f3c27501a6d856
ABRAHAM LEANDRO VEGA <leandrovegaabraham@gmail.com>
2022-12-08 11:19:19 -0500

    routes

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles/_main.scss'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home'
+import About from './routes/About'
+import Contact from './routes/Contact'
+import Error from './routes/Error'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: 'contacts/:contactid',
+        element: <Contact />
+      }
+    ]
+  },
+  {
+    path: '/about',
+    element: <About />
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
