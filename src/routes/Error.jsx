3fd28e772df436a519e23a61e0bd05160829828a
ABRAHAM LEANDRO VEGA <leandrovegaabraham@gmail.com>
2022-12-17 03:10:43 -0500

    added page error 404

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,12 +1,15 @@
-import { useRouteError } from 'react-router-dom'
+import Banner from '../components/Banner'
+import banner from '../data/error/banner.json'
 import Menu from '../components/Menu'
+import Footer from '../components/Footer'
 export default function Error() {
-  const error = useRouteError()
   return (
-    <div>
+    <>
       <Menu />
-      <h1>Error</h1>
-      <p>{error.statusText || error.message}</p>
-    </div>
+      <div className='project_section'>
+        <Banner {...banner} />        
+      </div>
+      <Footer />
+    </>
   )
 }
