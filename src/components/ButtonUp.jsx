fe7d3bfeeacb079b532d980243bb360d71832869
leasever <leandrovegaabraham@gmail.com>
2022-12-30 03:32:48 -0500

    deleted title

--- src/components/ButtonUp.jsx
+++ src/components/ButtonUp.jsx
@@ -1,29 +1,28 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 export default function ButtonUp() {
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false)
 
   window.onscroll = () => {
     document.documentElement.scrollTop > 700
       ? setIsVisible(true)
-      : setIsVisible(false);
-  };
+      : setIsVisible(false)
+  }
   const handleToUp = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth',
-    });
-  };
+      behavior: 'smooth'
+    })
+  }
 
   return (
     <>
       <div
         className={`button_up ${!isVisible ? ' ' : 'active_up'}`}
         onClick={handleToUp}
-        title="Ir al inicio"
       >
         <div className='arrow-right icon'></div>
       </div>
     </>
-  );
+  )
 }
